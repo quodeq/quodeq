5fec26ef71ff368da33542babb4d8d84fcfc01c5
Victor Purcallas Marchesi <vpurcallas@gmail.com>
2026-04-26 09:55:24 +0200

    feat(report-viewer): add ReportSplitButton with chevron menu

---
+++ src/quodeq/ui/src/features/report-viewer/index.js
@@ -0,0 +1,4 @@
+export { ReportViewerProvider } from './ReportViewerProvider.jsx';
+export { useReportViewer } from './ReportViewerContext.jsx';
+export { ReportPane } from './ReportPane.jsx';
+export { ReportSplitButton } from './ReportSplitButton.jsx';
