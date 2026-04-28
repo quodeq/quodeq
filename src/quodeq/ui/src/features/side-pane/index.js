4dd1abc02b6b162ecc16a3c60814ee806e31eba3
Victor Purcallas Marchesi <vpurcallas@gmail.com>
2026-04-28 19:44:13 +0200

    feat(side-pane): barrel index for the new feature module

---
+++ src/quodeq/ui/src/features/side-pane/index.js
@@ -0,0 +1,5 @@
+export { SidePane } from './SidePane.jsx';
+export { SidePaneProvider } from './SidePaneProvider.jsx';
+export { useSidePane } from './SidePaneContext.jsx';
+export { useRegisterWindowSpec } from './useRegisterWindowSpec.js';
+export { ReportContent } from './reportContent.jsx';
